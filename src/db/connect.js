5d38198c4564433a63404329ab09bee1cf26f2b6
AysKrimn <akabilmemne@gmail.com>
2024-07-08 18:18:50 +0300

    chore: variables linked with env

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose")
-const URI = process.env["MONGOURI"]
 
+const db_username = process.env.MONGO_INITDB_ROOT_USERNAME
+const db_password = process.env.MONGO_INITDB_ROOT_PASSWORD
+const db_host = process.env.DB_HOST
+const db_port = process.env.DB_PORT
+const db_atlas_uri = process.env.URI
 
 const make_db_connection = () => {
 
-        
-        mongoose.connect(URI).then(() => {
+        console.log("U:", db_username, "P:", db_password, "port:", db_port)
+        // const DB_URI = `mongodb://${db_username}:${db_password}@${db_host}:${db_port}/slotdb`
+        mongoose.connect(db_atlas_uri).then(() => {
 
             console.log("Connected to database successfully")
 
