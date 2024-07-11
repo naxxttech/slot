30ae3ff1c833f304aa6098c80e39e3ef279a35a2
AysKrimn <akabilmemne@gmail.com>
2024-07-11 17:22:32 +0300

    remove test_user variable

--- src/modules/ocean/routes/spin.js
+++ src/modules/ocean/routes/spin.js
@@ -3,16 +3,11 @@ const router = express.Router()
 const Game = require("@src/base/config")
 
 
-const test_user = {
-
-    name: "admin",
-    bucket: 150,
-}
 
 router.get("/spin", async (request, response) => { 
 
      const requested_gameId = 54
-     const requestedPaylines = 5
+     const requestedPaylines = 20
      const oceanGame = new Game(requested_gameId)
      const data = await oceanGame.generate_game_table(3, 5, requestedPaylines)
 
