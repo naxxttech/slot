94994f0ec53ee2a83ee738cd034e43563e958b4e
AysKrimn <akabilmemne@gmail.com>
2024-07-11 17:39:02 +0300

    import socket.io and create htpp server

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 require('module-alias/register')
 require('dotenv').config()
 
+
+const ngrok = require("ngrok")
 const express = require("express")
 const expressLayouts = require('express-ejs-layouts')
 const cors = require("cors")
+const path = require('path');
+
 
 const app = express()
 const port = process.env["PORT"]
+const server = http.createServer(app);
 // our db
 const connectdb = require("./db/connect")
-
+// our socket
+const createSocket = require("./io/socket")
 // static 
 app.use(express.static("public"))
-app.use("/css", express.static(__dirname + "public/css"))
-
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
 // middlewares
 app.use(cors())
 app.use(expressLayouts)
 // parser
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 // view
 app.set("view engine", "ejs")
 app.set("layout", "./layouts/base")
@@ -46,9 +52,27 @@ app.use("/admin", adminRouter)
 app.use(base_api_path, oceanAPI)
 
 
-app.listen(port, () => {
+const developmentMode = false
+
+server.listen(port, () => {
 
     console.log(`Server running on http://localhost:${port}/`)
     console.log(`API located at http://localhost:${port}${base_api_path}/<query_path>`)
     console.log(`Admin dashboard: http://localhost:${port}/admin`)
+
+    // start socket
+    createSocket(server)
+
+    if (developmentMode) {
+            // create tunnel
+            ngrok.connect(port).then(tunnelURL => {
+
+                console.log("PUBLIC TUNNEL:", tunnelURL)
+
+            }).catch(error => console.log("Tunnel error:", error))
+
+    }
+
 })
+
+
