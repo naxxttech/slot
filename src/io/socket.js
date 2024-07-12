6535fccd07d0e239ee61f76ec83e0dc45ce3c7ea
AysKrimn <akabilmemne@gmail.com>
2024-07-12 04:12:29 +0300

    feat: socket integration and its tests

--- src/io/socket.js
+++ src/io/socket.js
@@ -1,4 +1,5 @@
 const socket = require("socket.io")
+const Game = require("../base/config")
 
 const initializeSocket = (server) => {
         // socket config
@@ -14,11 +15,25 @@ const initializeSocket = (server) => {
 
         // events
         io.on("connection", (socket) => {
-            console.log("connected - connection id:", socket.id)
+            
+            console.log(`[${socket.id}] -`, "User just connected")
+
+
+            socket.on("spin", async (data) => {
+
+                console.log("SPIN RECEIVED:", data)
+                
+                const { requestedLines } = data
+                const requested_game = new Game(data.gameId)
+                const game_data = await requested_game.generate_game_table(3, 5, requestedLines)
+
+                socket.emit("result", game_data)
+            })
 
             socket.on("disconnect", () => {
-
-                console.log("User just disconnect")
+                // user disconnect for some reason create game history in here
+                console.log(`[${socket.id}] -`, "User just disconnect")
+            
             })
 
         })
