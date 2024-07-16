4fc88ed526a5571de101a8e506422791ac6b5df3
AysKrimn <akabilmemne@gmail.com>
2024-07-16 18:20:56 +0300

    chore: update variables

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose")
 
-const db_username = process.env.MONGO_INITDB_ROOT_USERNAME
-const db_password = process.env.MONGO_INITDB_ROOT_PASSWORD
-const db_host = process.env.DB_HOST
-const db_port = process.env.DB_PORT
-const db_atlas_uri = process.env.URI
+
+const db_atlas_uri = process.env.MONGO_URI_CLOUD
+const db_local_uri = process.env.MONGO_URI_DEV
+const db_test_uri = process.env.MONGO_URI_TESTSV
 
 const make_db_connection = () => {
 
-        console.log("U:", db_username, "P:", db_password, "port:", db_port)
         // const DB_URI = `mongodb://${db_username}:${db_password}@${db_host}:${db_port}/slotdb`
-        mongoose.connect(db_atlas_uri).then(() => {
+        mongoose.connect(db_local_uri).then(() => {
 
             console.log("Connected to database successfully")
 
