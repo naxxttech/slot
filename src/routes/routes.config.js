78050a0b56d5f368752df7e18f099e4d9ba67d21
AysKrimn <akabilmemne@gmail.com>
2024-07-16 14:36:09 +0300

    remove unused variables for now

--- src/routes/routes.config.js
+++ src/routes/routes.config.js
@@ -1,15 +1,18 @@
 const homeRouter = require("./home/index")
 const adminRouter = require("./admin/index")
-const gameRouter = require("./game/index")
+const gameRouter = require("./game/index");
+
+
+
 
 
 const configureRoutes = (app, prefix) => {
     
-    console.log("Adming page serves:", `${prefix}/admin`)
+    console.log("Adming page serves:", `${prefix}admin`)
 
     app.use('/', homeRouter);
-    app.use(`/${prefix}/admin`, adminRouter);
-    app.use(`/${prefix}/games`, gameRouter);
+    app.use(`${prefix}/admin`, adminRouter);
+    app.use(`${prefix}/games`, gameRouter);
 
 };
 
