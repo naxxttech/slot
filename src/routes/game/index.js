1b8488ba3e266d733964793fe18ee23359abc992
AysKrimn <akabilmemne@gmail.com>
2024-07-16 14:36:27 +0300

    .

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-const SlotGame = require("@src/base/game.config")
+// const SlotGame = require("@src/base/game.config")
+const SlotGame = require("../../base/game.config")
 
 router.get("/:gameId", async (request, response) => {
 
@@ -13,11 +14,16 @@ router.get("/:gameId", async (request, response) => {
         return response.status(400).json({ message: "No game ID specified."})
     }
  
-    const requestedPaylines = 25
-    const game = new SlotGame(requested_gameId)
-    const data = await game.generate_game_table(requestedPaylines)
+    // testId
+    const testGameId = 54
 
-    response.status(200).json(data)
+    const requestedPaylines = 25
+    const game = new SlotGame(testGameId)
+    const gameResult = await game.generate_game_table(requestedPaylines)
+
+    console.log("DATA:", gameResult)
+
+    response.status(200).json(gameResult)
 
 })
 
