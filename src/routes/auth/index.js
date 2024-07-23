fc30e4fa6e7b768af20c4b47ee9b49e5aa12d83d
AysKrimn <akabilmemne@gmail.com>
2024-07-23 13:07:03 +0300

    add test endpoint

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,12 +1,38 @@
 const express = require("express")
 const router = express.Router()
 
+const secureAuth = require("../../middlewares/secure-auth")
+
+router.get("/test", async (request, response) => {
+    console.log("USER?", request.session.user)
 
 
-router.get("/generate-session", async (request, response) => {
+    response.json(request.session.user || "no user")
 
-    // express session or random generated session?
-    response.status(201).json({ message: "Session created."})
+})
+
+router.post("/create-session", secureAuth, async (request, response) => {
+
+    const { bank, currency, id, nickname, externaltoken, gameid} = request.body
+    const response_object = { msg: "", sessionId: ""}
+
+    if (!bank || !currency || !id || !externaltoken || !gameid) {
+
+        response_object.msg = "Invalid or missing parameters."
+        return response.status(400).json(response_object)
+    }
+
+    console.log("USER?", request.session.user)
+
+    request.session.user = { bank, currency, id, externaltoken, gameid }
+
+    if (nickname) {
+            request.session.user.nickname = nickname
+    }
+
+    response_object.msg = "OK"
+    response_object.sessionId = request.sessionID
+    response.status(201).json(response_object)
 })
 
 
