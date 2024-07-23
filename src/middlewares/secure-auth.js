41fac4919b615702a830febee68a63aaf5d54b5a
AysKrimn <akabilmemne@gmail.com>
2024-07-23 13:06:33 +0300

    new middleware for checking auth header

---
+++ src/middlewares/secure-auth.js
@@ -0,0 +1,18 @@
+
+
+const authMiddleware = (request, response, next) => {
+    const authHeader = request.headers['auth']; 
+    const expectedAuthValue = process.env["AUTHHEADER"]
+    
+    if (authHeader === expectedAuthValue) {
+
+        next(); 
+
+    } else {
+        response.status(403).json({ message: 'Forbidden: Invalid auth token' });
+    }
+};
+
+
+
+module.exports = authMiddleware
