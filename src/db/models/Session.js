e971f98b356421b7ff3e4f1b384b9b871a2861b1
AysKrimn <akabilmemne@gmail.com>
2024-07-25 17:01:12 +0300

    update methods

--- src/db/models/Session.js
+++ src/db/models/Session.js
@@ -15,8 +15,8 @@ const SessionSchema = new Schema({
     user_id: { type: String, required: true},
     user_nickname: { type: String },
     externaltoken: { type: String, required: true},
-    gameid: { type: String, required: true}
-
+    gameid: { type: String, required: true},
+    expiredAt: { type: Date, default: new Date(Date.now() + 20 * 60 * 1000) }
 }, {
     timestamps: true
 })
@@ -91,5 +91,38 @@ const get_session = async (sessionId) => {
 }
 
 
+const get_active_session = async (body) => {
 
-module.exports = { create_session, get_session }
+    const response_object = { code: null, message: "", resource: null }
+
+    try {
+
+        const session = await model.findOne({ user_id: body.user_id})
+        // console.log("received body:", body)
+        console.log("Found session:", session)
+
+        if (!session) {
+
+            response_object.code = 404
+            response_object.message = "Session is not found or expired"
+            return response_object
+        }
+
+        response_object.code = 200
+        response_object.message = "OK"
+        response_object.resource = session
+        
+    } catch (error) {
+        
+        console.log("[Find Session] Error:", error)
+
+        response_object.code = 500
+        response_object.message = "Something went wrong"
+    }
+ 
+    
+    return response_object
+}
+
+
+module.exports = { create_session, get_session, get_active_session }
