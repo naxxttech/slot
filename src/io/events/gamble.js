df29703e313d84d32899e4d00a827f51e72cea34
AysKrimn <akabilmemne@gmail.com>
2024-07-30 21:46:40 +0300

    add gamble feature

--- src/io/events/gamble.js
+++ src/io/events/gamble.js
@@ -1,17 +1,14 @@
-const PaymentProcessor = require("../../base/payment.config")
 const { update_game_history } = require("../../db/models/GameHistory")
 const handleErrorsIfAny = require("../helpers/handle.socket.errors")
 
 
-const gambleChosed = (socket) => {
+const triggerGamble = (socket) => {
 
 
     socket.on("canGamble", async (data, cb) => {
         
          handleErrorsIfAny(async () => {
 
-
-         const { user_id, gameid } = socket.request.session
          const { entryId } = data
 
          const operationResult = await update_game_history(entryId, "gamble")
@@ -27,4 +24,4 @@ const gambleChosed = (socket) => {
 
 
 
-module.exports = gambleChosed
+module.exports = triggerGamble
